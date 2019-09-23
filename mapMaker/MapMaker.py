5de207086b2225b29cddb43a11fe649a4f1504ba
Christopher Loonam <cloonam@purdue.edu>
2019-09-23 11:13:09 -0400

    map

--- mapMaker/MapMaker.py
+++ mapMaker/MapMaker.py
@@ -8,22 +8,29 @@ from PyQt5.QtCore import QLineF, Qt
 from MapMakerUi import Ui_MapMakerWindow
 import numpy as np
 import struct
+from enum import Enum
+
+class DrawingMode(Enum):
+    Line = 1
+    Arc = 2
 
 class MapScene(QGraphicsScene):
     def __init__(self, viewWidth, viewHeight, width=10, height=10):
         super(MapScene, self).__init__()
-        self.widthStep = viewWidth / width
-        self.heightStep = viewHeight / height
+        self.widthStep = viewWidth / (width + 0.5)
+        self.heightStep = viewHeight / (height + 0.5)
         self.width = width
         self.height = height
         self.path = None
+        self.mode = DrawingMode.Line
 
         self.shapes = []
         self.currentShape = []
+        self.arc = []
         self.drawRects()
 
-    def drawLine(self, p1, p2):
-        pen = QPen(QColor(255, 0, 0))
+    def drawLine(self, p1, p2, color=QColor(0, 255, 0)):
+        pen = QPen(color)
         pen.setWidth(1)
         x1 = p1[0]*self.widthStep
         x2 = p2[0]*self.widthStep
@@ -43,23 +50,57 @@ class MapScene(QGraphicsScene):
                 self.addRect(x, y, self.widthStep, self.heightStep)
                 y += self.heightStep
             x += self.widthStep
-        for shape in self.shapes + [self.currentShape]:
+        for shape in self.shapes:
             for i, p in enumerate(shape):
-                self.addEllipse(p[0]*self.widthStep, p[1]*self.heightStep, 5, 5)
+                self.addEllipse(p[0]*self.widthStep-1.5, p[1]*self.heightStep-1.5, 3, 3)
                 if i != 0:
                     self.drawLine(shape[i-1], p)
+        for i, p in enumerate(self.currentShape):
+            self.addEllipse(p[0]*self.widthStep-1.5, p[1]*self.heightStep-1.5, 3, 3)
+            if i != 0:
+                self.drawLine(self.currentShape[i-1], p, QColor(255, 0, 0))
+        for p in self.arc:
+            self.addEllipse(p[0]*self.widthStep-1.5, p[1]*self.heightStep-1.5, 3, 3)
+
+    def addArc(self):
+        p1 = self.arc[0]
+        p2 = self.arc[1]
+        mid = self.arc[2]
+        res = 8
+        self.currentShape.append(p1)
+        for i in range(res):
+            x = p1[0] + (mid[0] - p1[0])/(i+1)
+            y = p1[1] + (mid[1] - p1[1])/(i+1)
+            self.currentShape.append((x, y))
+        self.currentShape.append(mid)
+        for i in range(res):
+            x = mid[0] + (p2[0] - mid[0])/(i+1)
+            y = mid[1] + (p2[1] - mid[1])/(i+1)
+            self.currentShape.append((x, y))
+        self.currentShape.append(p2)
 
     def mousePressEvent(self, mousePressEvent: QMouseEvent):
         point = mousePressEvent.scenePos()
         x = point.x() / self.widthStep
         y = point.y() / self.heightStep
-        self.currentShape.append((x, y))
-        self.drawRects()
+        if self.mode == DrawingMode.Line:
+            self.arc = []
+            self.currentShape.append((x, y))
+            self.drawRects()
+        elif self.mode == DrawingMode.Arc:
+            if len(self.arc) < 2:
+                self.arc.append((x, y))
+            else:
+                self.arc.append((x, y))
+                self.addArc()
+                self.arc = []
+            self.drawRects()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Return:
             self.shapes.append(self.currentShape)
             self.currentShape = []
+            self.drawRects()
 
     def saveToPath(self, filePath):
         with open(filePath, 'wb') as f:
@@ -77,13 +118,20 @@ class MapMaker(QMainWindow, Ui_MapMakerWindow):
         self.setupUi(self)
 
         self.saveButton.clicked.connect(self.save)
-#        self.calcButton.clicked.connect(self.calc)
+        self.lineButton.clicked.connect(self.setLine)
+        self.arcButton.clicked.connect(self.setArc)
         self.widthEntry.setText('10')
         self.heightEntry.setText('10')
         self.widthEntry.returnPressed.connect(self.drawGrid)
         self.heightEntry.returnPressed.connect(self.drawGrid)
         self.widthEntry.editingFinished.connect(self.drawGrid)
         self.heightEntry.editingFinished.connect(self.drawGrid)
+
+    def setLine(self):
+        self.gridView.scene().mode = DrawingMode.Line
+
+    def setArc(self):
+        self.gridView.scene().mode = DrawingMode.Arc
     
     def save(self):
         fileName = QFileDialog.getSaveFileName(self, 'File')
